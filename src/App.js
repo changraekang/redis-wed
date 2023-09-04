d1e9a68997266897bb0efdbd03ea29190db6007a
changraekang <storkcr@gmail.com>
2023-09-04 22:28:51 +0900

    init redis

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css"; // CSS 파일을 import
 
-function App() {
+const App = () => {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [responseMessage, setResponseMessage] = useState("");
+
+  const handleSubmit = async () => {
+    try {
+      const response = await axios.post("http://localhost:3000/post-endpoint", {
+        name,
+        age,
+      });
+
+      setResponseMessage(response.data.message);
+    } catch (error) {
+      console.error("There was an error sending the POST request", error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <h1>Send a POST Request</h1>
+      <input
+        className="input-field"
+        type="text"
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        className="input-field"
+        type="text"
+        placeholder="Age"
+        value={age}
+        onChange={(e) => setAge(e.target.value)}
+      />
+      <button className="submit-button" onClick={handleSubmit}>
+        Submit
+      </button>
+      <p className="response-message">{responseMessage}</p>
     </div>
   );
-}
+};
 
 export default App;
